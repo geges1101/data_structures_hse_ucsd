0205ee3143a3ddd035be06fe2cd04fba8642d4f9
geges1101 <62469977+geges1101@users.noreply.github.com>
2021-04-01 20:17:09 +0300

    Add files via upload

---
+++ Week 6 Assignments/isBST/src/com/company/is_bst.java
@@ -0,0 +1,88 @@
+//package com.company;
+
+import java.util.*;
+import java.io.*;
+
+public class is_bst {
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public class IsBST {
+        class Node {
+            int key;
+            int left;
+            int right;
+
+            Node(int key, int left, int right) {
+                this.left = left;
+                this.right = right;
+                this.key = key;
+            }
+        }
+
+        int nodes;
+        Node[] tree;
+
+        void read() throws IOException {
+            FastScanner in = new FastScanner();
+            nodes = in.nextInt();
+            tree = new Node[nodes];
+            for (int i = 0; i < nodes; i++) {
+                tree[i] = new Node(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+        }
+
+        boolean isBinarySearchTree() {
+            // Implement correct algorithm herу
+            if(tree.length == 0) return true;
+            Node root = tree[0];
+            return checkBST(0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+
+        boolean checkBST(int x, int min, int max) {
+            if (x < 0)	return true;
+            if (tree[x].key > max || tree[x].key < min)
+                return false;
+
+            return checkBST(tree[x].left, min, tree[x].key - 1)
+                    && checkBST(tree[x].right, tree[x].key + 1, max);
+        }
+    }
+
+
+
+    static public void main(String[] args) throws IOException {
+        new Thread(null, new Runnable() {
+            public void run() {
+                try {
+                    new is_bst().run();
+                } catch (IOException e) {
+                }
+            }
+        }, "1", 1 << 26).start();
+    }
+    public void run() throws IOException {
+        IsBST tree = new IsBST();
+        tree.read();
+        if (tree.isBinarySearchTree()) {
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
+        }
+    }
+}
